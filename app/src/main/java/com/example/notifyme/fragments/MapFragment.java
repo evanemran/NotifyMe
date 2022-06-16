5cc62d00b3b964d4b65eddf3c3cb601bcbf6c124
Emran Khandaker Evan <evanemran007@gmail.com>
2022-06-16 08:39:52 +0600

    own address tracker added

--- app/src/main/java/com/example/notifyme/fragments/MapFragment.java
+++ app/src/main/java/com/example/notifyme/fragments/MapFragment.java
@@ -6,11 +6,15 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -30,6 +34,10 @@ import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     View view;
@@ -37,11 +45,14 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     SupportMapFragment mapFragment;
     private FusedLocationProviderClient client;
     private Marker MyMarker;
+    TextView textView_my_location;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        textView_my_location = view.findViewById(R.id.textView_my_location);
 
         requestPermission();
 
@@ -88,6 +99,8 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                             googleMap.setMyLocationEnabled(true);
                             final LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
                             map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,16));
+                            textView_my_location.setText(getAddress(location.getLatitude(), location.getLongitude()));
+                            textView_my_location.setSelected(true);
                         }
 
                     }
@@ -101,5 +114,35 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     private void requestPermission() {
         ActivityCompat.requestPermissions(getActivity(), new String[]{ACCESS_FINE_LOCATION}, 1);
+    }
+
+    public String getAddress(double lat, double lng) {
+        String address = "";
+        Geocoder geocoder = new Geocoder(getContext(), Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(lat, lng, 1);
+            Address obj = addresses.get(0);
+            String add = obj.getAddressLine(0);
+            add = add + "\n" + obj.getCountryName();
+            add = add + "\n" + obj.getCountryCode();
+            add = add + "\n" + obj.getAdminArea();
+            add = add + "\n" + obj.getPostalCode();
+            add = add + "\n" + obj.getSubAdminArea();
+            add = add + "\n" + obj.getLocality();
+            add = add + "\n" + obj.getSubThoroughfare();
+
+            address = address + " " + obj.getAddressLine(0);
+            Log.v("IGA", "Address" + add);
+            // Toast.makeText(this, "Address=>" + add,
+            // Toast.LENGTH_SHORT).show();
+
+            // TennisAppActivity.showDialog(add);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+
+        return address;
     }
 }
